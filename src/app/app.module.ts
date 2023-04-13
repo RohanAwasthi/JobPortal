b8a898ba2cee89a5e886c115689429da858613b5
RohanAwasthi <128706839+RohanAwasthi@users.noreply.github.com>
2023-04-13 17:46:31 +0530

    commit 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+
 import { JoblistingComponent } from './components/joblisting/joblisting.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoute:Routes=[
+  {path:'Home', component:LoginComponent},
+  {path:'Register', component:RegisterComponent},
+  {path:'JobListing',component:JoblistingComponent}
+]
 
 @NgModule({
   declarations: [
+    NavbarComponent,
     AppComponent,
     LoginComponent,
-    RegisterComponent,
-    JoblistingComponent,
-    NavbarComponent
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
